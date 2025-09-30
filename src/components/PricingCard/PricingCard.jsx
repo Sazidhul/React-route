3e4bd96d1c1930389318a68e650e24fe1c7d8161
Sazidhul <sazisazidhul@gmail.com>
2025-09-30 12:03:27 +0600

    Use Daisy UI pricing card to modify for the component

---
+++ src/components/PricingCard/PricingCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PricingFeature from './PricingFeature';
+
+const PricingCard = ({pricing}) => {
+   const {name, price, description, features} = pricing; //   for this destruction we can use each element individual 
+   return (
+      <div className='border bg-amber-900 rounded-2xl p-4'>
+         {/* card header */}
+         <div >
+            <h1 className='text-7xl'>{name}</h1>
+            <h4>{pricing.price}</h4>
+         </div>
+         {/* Card body */}
+         <div className='bg-blue-950 p-4 rounded-2xl mt-10'>
+            <p>{pricing.description}</p>
+            {
+               features.map((feature, index) => <PricingFeature key={index} feature={feature}></PricingFeature>)
+            }
+         </div>
+      </div>
+   );
+};
+
+export default PricingCard;
--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import PricingCard from '../PricingCard/PricingCard';
 
 const PricingOptions = ({pricingPromise}) => {
 
@@ -8,7 +9,12 @@ const PricingOptions = ({pricingPromise}) => {
 
    return (
       <div>
-         
+         <h2 className='text-5xl'>Get our Membership</h2>
+         <div className='grid md:grid-cols-3 gap-8'>
+            {
+               pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+            }
+         </div>
       </div>
    );
 };
